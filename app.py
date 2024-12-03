de612dec0c51df1a8d16c57266ef0ba2d2d11274
Rayan MOKRANE <rayan13170@protonmail.com>
2024-12-03 12:16:14 +0100

    Better error handling

--- app.py
+++ app.py
@@ -13,50 +13,73 @@ app = Flask(__name__)
 def home():
     not_found_projets = []
     found_projects = []
-    
-    try:
-        PROJECTS = load_projects_list_from_config()
-    except Exception:
-        not_found_projets.append("Impossible de charger le fichier de configuration, une erreur est survenue.")
-        PROJECTS = []
-    
+    config_error = None
+
+    success, PROJECTS = load_projects_list_from_config()
+
+    if not success:
+        config_error = PROJECTS[0]
+
+        return render_template(
+        "index.html",
+        projects=[],
+        project_folders_errors=", ".join(not_found_projets),
+        config_error=config_error,
+    )
+
     for project in PROJECTS:
-        if not os.path.isdir(project) or not os.path.exists(project) or not os.path.exists(os.path.join(project, "logs")):
+        if (
+            not os.path.isdir(project)
+            or not os.path.exists(project)
+            or not os.path.exists(os.path.join(project, "logs"))
+        ):
             not_found_projets.append(project)
         else:
             found_projects.append(project)
-    
+
     not_found_projets = [f'"{p}"' for p in not_found_projets]
     projects = [(p, os.path.basename(p)) for p in found_projects]
-    return render_template("index.html", projects=projects, project_folders_errors=", ".join(not_found_projets))
-
+    return render_template(
+        "index.html",
+        projects=projects,
+        project_folders_errors=", ".join(not_found_projets),
+        config_error=config_error,
+    )
 
 
 @app.route("/get_logs", methods=["POST"])
 def get_logs():
     data = request.get_json()
     project = data.get("project")
-    
+
     if not project:
         return jsonify({"success": False, "message": "Project not found"}), 400
-    
+
     LOG_DIR = os.path.join(project, "logs")
-    
+
     if not os.path.exists(LOG_DIR):
-        return jsonify({"success": False, "message": "This project has no logs folder"}), 400
-    
+        return (
+            jsonify({"success": False, "message": "This project has no logs folder"}),
+            400,
+        )
+
     log_files = [f for f in os.listdir(LOG_DIR) if f.endswith(".log")]
-    
+
     if not log_files:
-        return jsonify({"success": False, "message": "No log files found in this project"}), 400
-    
+        return (
+            jsonify(
+                {"success": False, "message": "No log files found in this project"}
+            ),
+            400,
+        )
+
     logs = ""
-    
+
     for log in log_files:
         with open(os.path.join(LOG_DIR, log)) as f:
             log_content = f.readlines()
             logs += "\n\n".join([line.strip() for line in log_content])
-    
+
     # Convert to DataFrame if needed
     as_df = data.get("as_df", False)
     if as_df:
@@ -74,29 +97,32 @@ def convert_to_df(logs: str):
         logs (str): Les logs au format str.
     """
     # Convert the logs into a list of lines
-    log_lines = logs.strip().split('\n')
+    log_lines = logs.strip().split("\n")
 
     # Parse the log lines into a structured format
     data = []
     for line in log_lines:
-        if '---------------' in line:
+        if "---------------" in line:
             continue  # Skip dashed lines
-        parts = line.split(' - ')
+        parts = line.split(" - ")
         if len(parts) == 7:
             date_time, level, server, session_id, duration, script, status = parts
-            data.append({
-                'Date_Time': date_time,
-                'Level': level,
-                'Host': server,
-                'Run_ID': session_id,
-                'Duration': float(duration),
-                'Script': script,
-                'Status': status
-            })
+            data.append(
+                {
+                    "Date_Time": date_time,
+                    "Level": level,
+                    "Host": server,
+                    "Run_ID": session_id,
+                    "Duration": float(duration),
+                    "Script": script,
+                    "Status": status,
+                }
+            )
 
     # Create a DataFrame
     df = pd.DataFrame(data)
     print(df)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
--- helpers.py
+++ helpers.py
@@ -1,21 +1,19 @@
 import json
 
+from typing import Tuple
+
 CONFIG_FILE = "projects.json"
 
 
-def load_projects_list_from_config() -> list:
+def load_projects_list_from_config() -> Tuple[bool, list]:
     """
     Méthode permettant de charger et retourner
     depuis un fichier de configuration la liste des projets pour
     lesquels on souhaite étudier les logs.
 
-    Raises:
-        Exception: Lorsqu'une variable du fichier de configuration est manquante.
-        FileNotFoundError: Lorsque le fichier de configuration n'est pas trouvé ou n'existe pas.
-        json.JSONDecodeError: Lorsque le contenu du fichier de configuration est invalide.
-
     Returns:
-        list: La liste des variables de configuration, dans le même ordre que la liste MANDATORY_VARS.
+        Tuple[bool, list]: Si succès, True + liste de projets
+                           Sinon, False + message d'erreur
     """
     try:
 
@@ -23,18 +21,17 @@ def load_projects_list_from_config() -> list:
             config = json.load(file)
             projects = config.get("projects")
             if projects is None:
-                raise Exception(
-                    f'{CONFIG_FILE} ne contient pas la variable "projects".'
-                )
+                return False, [f'{CONFIG_FILE} ne contient pas la variable "projects".']
 
-            return projects
+            return True, projects
 
     except FileNotFoundError:
-        raise FileNotFoundError(
-            f"{CONFIG_FILE} n'existe pas. Merci de créer le fichier de configuration."
+        return (
+            False,
+            [
+                f"{CONFIG_FILE} n'existe pas. Merci de créer le fichier de configuration."
+            ],
         )
 
     except json.JSONDecodeError:
-        raise json.JSONDecodeError(
-            f"Le format du fichier JSON est incorrect. {CONFIG_FILE}"
-        )
+        return False, ["Le format du fichier JSON est incorrect."]
